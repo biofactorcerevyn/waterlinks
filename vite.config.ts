742b6d2c6f200489517bba11eacc1be84a73515c
saichandugedela <toxichaste5@gmail.com>
2025-12-26 12:06:31 +0530

    fix_deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: "/WaterLinks_web/",
+    base: "/",
     
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
